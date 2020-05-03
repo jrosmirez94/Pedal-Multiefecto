65f27e30fd4f07b18172423d62980b611886cd9a
jrosmirez94 <jorge.rosmirez@gmail.com>
2020-05-03 19:11:29 -0300

    Agrego proyecto keil vacio

---
+++ Pedal para guitarra - Jorge Rosmirez _v00/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Pedal para guitarra - Jorge Rosmirez _v00' 
+ * Target:  'Pedal para guitarra - Jorge Rosmirez _v00' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
